ce16b1d54f30f134e23241b61a72f6629a68536a
KyryloKalinichenko <kirillstomper@gmail.com>
2020-12-03 16:48:50 +0100

    good one, but norm

--- get_next_line.h
+++ get_next_line.h
@@ -2,7 +2,22 @@
 #ifndef GET_NEXT_LINE_H
 #define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 10
+# define BUFF_SIZE 1000000
+#include <stdlib.h>
+
+typedef struct			s_list
+{
+	void		*content;
+	struct s_list	*next;
+}				t_list;
+
 int     get_next_line(int fd, char **line);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strdup(const char *s);
+char		*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
+char		*ft_strncpy(char *dest, const char *src, size_t n);
+char	*ft_strcat(char *dest, const char *src);
 
 #endif
--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -84,3 +84,4 @@ size_t	ft_strlen(const char *s)
 		i++;
 	return (i);
 }
+
